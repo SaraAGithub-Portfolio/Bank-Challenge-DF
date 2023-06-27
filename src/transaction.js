0a810611593b70182c04b1a4ee478457d900d498
SaraAGithub-Portfolio <skausa29@gmail.com>
2023-06-27 09:55:40 +0100

    cleaned up src transaction file. Made properties private. Changed method names to reflect acceptance criteria.

--- src/transaction.js
+++ src/transaction.js
@@ -1,25 +1,27 @@
 class Transaction {
-    constructor(date, deposit, withdrawal, balance) {
-        if (!date || !deposit || !balance) {
-            throw new Error('Invalid input');
-        }
-        this._date = date;
-        this._deposit = deposit;
-        this._withdrawal = withdrawal;
-        this._balance = balance;
-    }
-    get date() {
-        return this._date;
-    }
-    get balance() {
-        return this._balance;
-    }
-    toString() {
-        return `${this._date} || $${this._deposit.toFixed(2)} || ${this._withdrawal ? this._withdrawal.toFixed(2) : ''} || ${this._balance.toFixed(2)}`;
-    }
-    calculateBalance() {
-        return this._balance + (this._deposit || 0) - (this._withdrawal || 0);
-    }
-}
+    #date;
+    #credit;
+    #debit;
+    #balance;
 
+    constructor(date = '', credit = 0, debit = 0, balance = 0) {
+        this.#date = date;
+        this.#credit = credit;
+        this.#debit = debit;
+        this.#balance = balance;
+    };
+    getDate() {
+        return this.#date;
+    }
+    getDebit() {
+        return this.#debit;
+    }
+    getCredit() {
+        return this.#credit;
+    }
+    getBalance() {
+        return this.#balance;
+    }
+
+}
 export default Transaction;
