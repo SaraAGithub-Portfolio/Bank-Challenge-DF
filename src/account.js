cb394f84ff7886bab2b894ff37c76c8e83032874
SaraAGithub-Portfolio <skausa29@gmail.com>
2023-06-27 09:50:33 +0100

    Previously my properties had an _ but I needed to make them private so I changed my properties to accountBalance and transactionArray and added # to them. Tried to incorporate error handling with NaN throw statements. May refractor to add more error handling.

--- src/account.js
+++ src/account.js
@@ -1,40 +1,39 @@
 class Account {
-    _balance = 0;
-    _transactions = [];
-    constructor() {
-        this._balance = 0;
-        this._transactions = [];
+    #accountBalance;
+    #transactionArray = [];
+
+    constructor(initialBalance = 0, transactions = []) {
+        this.#accountBalance = initialBalance;
+        this.#transactionArray = transactions;
+    }
+    hasAccount() {
+        return true;
     }
     getBalance() {
-        return this._balance;
-    }
-    isANumber(amount) {
-        return typeof amount === 'number';
-    }
-    deposit(amount) {
-        if (!this.isANumber(amount)) {
-            throw new Error('This is not a number');
-        }
-        this._balance += amount;
-    }
-    isDuplicateTransaction(transaction) {
-        return this._transactions.some(
-            (existingTransaction) =>
-                existingTransaction.date === transaction.date && existingTransaction.deposit === transaction.deposit && existingTransaction.withdrawal === transaction.withdrawal && existingTransaction.balance === transaction.balance
-        );
-    }
-    addTransaction(transaction) {
-        if (this.isDuplicateTransaction(transaction)) {
-            throw new Error('Duplicate transaction');
-        }
-        this._transactions.push(transaction);
+        return this.#accountBalance;
     }
     getTransactions() {
-        return this._transactions;
+        return this.#transactionArray;
     }
-    printStatement(statementPrinter) {
-        statementPrinter.printerStatement(this._transactions);
+    withdraw(transactions) {
+        const date = transactions.getDate();
+        const credit = transactions.getDeposit();
+        const debit = transactions.getWithdraw();
+        if (isNaN(debit)) {
+            throw new Error('Invalid input: withdraw amount is not a number')
+        }
+        this.#accountBalance -= debit;
+        return this.#transactionArray.push(transactions)
+    };
+    deposit(transactions) {
+        const date = transactions.getDate();
+        const credit = transactions.getDeposit();
+        const debit = transactions.getWithdraw();
+        if (isNaN(credit)) {
+            throw Error('Invalid input: deposit amount is not a number');
+        }
+        this.#accountBalance += credit;
+        return this.#transactionArray.push(transactions)
     }
-
 }
 export default Account;
