bbbcdc44ee5e26179e4c241f75fe6d57af6e228c
SaraAGithub-Portfolio <skausa29@gmail.com>
2023-06-27 09:54:42 +0100

    Cleaned up  my transaction spec file to run passing tests on  Transaction Tests  to check that the getters are retrieving the correct information. Needed to adjust the wording so that it reflected the acceptance criteria (i.e. deposit -> credit). Added SpyOn function to spy on the data being retrieved.

--- spec/support/transactionspec.js
+++ spec/support/transactionspec.js
@@ -1,42 +1,40 @@
-import Transaction from "../../src/transaction.js";
+import Transaction from "../../src/transaction.js"
 
 describe('Transaction tests', () => {
-    let testTransaction;
+    let transaction;
     beforeEach(() => {
-        testTransaction = new Transaction('10/01/2012', 1000, null, 1000);
+        transaction = new Transaction('11/11/2011', 100, 50, 50);
     });
-    it('should create a transaction with valid data', () => {
-        expect(testTransaction).toBeInstanceOf(Transaction);
-        expect(testTransaction._date).toBe('10/01/2012');
-        expect(testTransaction._deposit).toBe(1000);
-        expect(testTransaction._withdrawal).toBeNull();
-        expect(testTransaction._balance).toBe(1000);
+    it('should have the correct date', () => {
+        expect(transaction.getDate()).toEqual('11/11/2011');
     });
-    it('should throw an error for an invalid input', () => {
-        expect(() => {
-            new Transaction(null, 1000, null, 1000);
-        }).toThrowError(Error, 'Invalid input');
+    it('should call the getDate method', () => {
+        spyOn(transaction, 'getDate');
+        transaction.getDate();
+        expect(transaction.getDate).toHaveBeenCalledWith();
     });
-
-    it('should determine the correct transaction type', () => {
-        const depositTransaction = new Transaction('13/01/2012', 2000, null, 1000);
-        expect(depositTransaction._withdrawal).toBeNull();
+    it('should have the correct debit amount', () => {
+        expect(transaction.getDebit()).toEqual(50);
     });
-    it('should calculate the balance correctly', () => {
-        const transaction1 = new Transaction('10/01/2012', 1000, null, 1000);
-        expect(transaction1._balance).toBe(1000);
+    it('should call the getDebit method', () => {
+        spyOn(transaction, 'getDebit');
+        transaction.getDebit();
+        expect(transaction.getDebit).toHaveBeenCalledWith();
     });
-    it('should handle date formatting correctly', () => {
-        const transaction = new Transaction('01-10-2012', 1000, null, 1000);
-        expect(transaction._date).toBe('01-10-2012');
+    it('should have the correct credit amount', () => {
+        expect(transaction.getCredit()).toEqual(100)
     });
-    it('should have properties', () => {
-        expect(testTransaction).toBeDefined();
-        expect(testTransaction).toBeDefined();
-        expect(testTransaction).toBeDefined();
+    it('should call the getCredit method', () => {
+        spyOn(transaction, 'getCredit');
+        transaction.getCredit();
+        expect(transaction.getCredit).toHaveBeenCalledWith();
     });
-    it('should be printed correctly', () => {
-        expect(testTransaction.toString()).toBe('10/01/2012 || $1000.00 ||  || 1000.00');
-
+    it('should have the correct balance', () => {
+        expect(transaction.getBalance()).toEqual(50);
+    });
+    it('should call on the getBalance method', () => {
+        spyOn(transaction, 'getBalance');
+        transaction.getBalance();
+        expect(transaction.getBalance).toHaveBeenCalledWith();
     });
 });
