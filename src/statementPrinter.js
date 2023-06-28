3c721dde191c9ac11504b825d3934e0c20f08599
SaraAGithub-Portfolio <skausa29@gmail.com>
2023-06-28 11:01:03 +0100

    adjusted the code in the statement printer src file to accommodate changes made in spec file.Used padEnd method to fix the spacing issuing for when the statement prints to the console so that everything is aligned correctly.

--- src/statementPrinter.js
+++ src/statementPrinter.js
@@ -1,10 +1,15 @@
-import chalk from "chalk";
 
 class StatementPrinter {
-    static printArray(strArray) {
-        strArray.forEach(str => console.log(str));
+    static printArray(transactions) {
+        transactions.forEach(transaction => {
+            const date = transaction.getDate();
+            const credit = transaction.getCredit() !== null ? transaction.getCredit().toFixed(2) : '';
+            const debit = transaction.getDebit() !== null ? transaction.getDebit().toFixed(2) : '';
+            const balance = transaction.getBalance().toFixed(2);
+            console.log(`${date} || ${credit.padEnd(7)} || ${debit.padEnd(6)} || ${balance}`);
+
+        });
     }
 }
-
 
 export default StatementPrinter; 
