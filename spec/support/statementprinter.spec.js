650ea5b2d862cf20a39ac5d625be4333b9e97c82
SaraAGithub-Portfolio <skausa29@gmail.com>
2023-06-28 11:55:18 +0100

    after implementing the extended criteria, I then had to refactor to accommodate the changes. I needed to specify a formatted criteria that checks for if it's a credit it should be colored green, debit red, and if the balance is less than zero red, grather than zero green.

--- spec/support/statementprinter.spec.js
+++ spec/support/statementprinter.spec.js
@@ -1,4 +1,5 @@
 import StatementPrinter from "../../src/statementPrinter.js";
+import chalk from "chalk";
 class MockTransaction {
     date;
     credit;
@@ -44,15 +45,20 @@ describe('Statement Printer tests', () => {
         expect(logSpy).toHaveBeenCalledTimes(transactionsArray.length);
     });
     it('should call console.log with the correct arguments', () => {
-
+        StatementPrinter.printArray(transactionsArray);
         transactionsArray.forEach((transaction) => {
-
-            StatementPrinter.printArray(transactionsArray);
             const date = transaction.getDate();
             const credit = transaction.getCredit() !== null ? transaction.getCredit().toFixed(2) : '';
             const debit = transaction.getDebit() !== null ? transaction.getDebit().toFixed(2) : '';
             const balance = transaction.getBalance().toFixed(2);
-            expect(logSpy).toHaveBeenCalledWith(`${date} || ${credit.padEnd(7)} || ${debit.padEnd(6)} || ${balance}`);
+
+            const formattedCredit = credit ? chalk.green(credit) : '';
+            const formattedDebit = debit ? chalk.red(debit) : '';
+            const formattedBalance = balance >= 0 ? chalk.green(balance) : chalk.red(balance);
+
+
+            const expectedOutput = (`${date} || ${formattedCredit.padEnd(7)} || ${formattedDebit.padEnd(6)} || ${formattedBalance}`);
+            expect(logSpy).toHaveBeenCalledWith(expectedOutput);
         });
     });
 });
