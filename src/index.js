14bcb6f112bf5160fe42433235d06de968165679
SaraAGithub-Portfolio <skausa29@gmail.com>
2023-06-28 11:05:36 +0100

    tested code and imported all 3 src files into the index file and tested the transactions and methods. when typing into the terminal node index.js the output was the acceptance criteria.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ import Account from "./account.js";
 import StatementPrinter from "./statementPrinter.js";
 import Transaction from "./transaction.js";
 
-const transactions = [
-    new Transaction('10/01/2012', 1000, null, 1000)
-];
 const account = new Account();
-transactions.forEach(transaction => account.addTransaction(transaction));
-StatementPrinter.print(account.getTransactions());
+
+const tran1 = new Transaction('14/01/2012', null, 500.00, 2500.00);
+const tran2 = new Transaction('13/01/2012', 2000.00, null, 3000.00);
+const tran3 = new Transaction('10/01/2012', 1000.00, null, 1000.00);
+
+account.withdraw(tran1);
+account.deposit(tran2);
+account.deposit(tran3);
+
+console.log('date       || credit  || debit  || balance');
+
+StatementPrinter.printArray(account.getTransactions());
+
+
+
