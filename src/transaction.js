0b7d23ce4d532650da2af7917e97b6ba83bd8a4e
SaraAGithub-Portfolio <skausa29@gmail.com>
2023-06-28 11:02:56 +0100

    adjusted transaction class code to reflect the error handler tests conducted in the transaction spec file. Also included setters.

--- src/transaction.js
+++ src/transaction.js
@@ -13,14 +13,32 @@ class Transaction {
     getDate() {
         return this.#date;
     }
+    setDate(date) {
+        this.#date = date;
+    }
     getDebit() {
         return this.#debit;
     }
+    setDebit(debit) {
+        if (debit < 0) {
+            throw new Error('Invalid Input: cannot be negative number');
+        }
+        this.#debit = debit;
+    };
     getCredit() {
         return this.#credit;
     }
+    setCredit(credit) {
+        if (isNaN(credit)) {
+            throw new Error('Invalid Input: must be a number');
+        }
+        this.#credit = credit;
+    }
     getBalance() {
         return this.#balance;
+    }
+    setBalance(balance) {
+        this.#balance = balance;
     }
 
 }
