f97c01dfc7e3d20ea1baee42bd1f86b0c3e892f5
SaraAGithub-Portfolio <skausa29@gmail.com>
2023-06-28 11:04:09 +0100

    Updated error handler file to reflect errors that should be caught and handled in the transaction class file as well. Tested the error handler by running the code. Code worked.

--- src/errorhandler.js
+++ src/errorhandler.js
@@ -1,20 +1,46 @@
-//try catch
 import Account from "./account.js";
+import Transaction from "./transaction.js";
 
-const account1 = new Account();
-isDuplicateTransaction(transaction)
+const account = new Account();
+
 try {
-    account1.isDuplicateTransaction(transaction)
+    const transactions = {
+        getDate: () => '11/11/2011',
+        getCredit: () => 100,
+        getDebit: () => 0
+    };
+    account.deposit(transactions);
 } catch (error) {
     console.log(error);
-    console.log('Duplicate transaction');
-    throw error;
-}
-addTransaction(transaction)
-try {
-    account1.addTransaction(transaction)
-} catch (error) {
-    console.log(error);
-    throw error;
+    console.log(`Deposit failed: ${error.message}`);
 }
 
+try {
+    const transactions = {
+        getDate: () => '11/11/2011',
+        getCredit: () => 100,
+        getDebit: () => NaN
+    };
+
+    account.withdraw(transactions);
+} catch (error) {
+    console.log(error);
+    console.log(`Withdraw failed: ${error.message}`);
+}
+try {
+    const tran1 = new Transaction('14/01/2012', null, 100.00, 200.00);
+    account.withdraw(tran1);
+} catch (error) {
+    console.log(error);
+    console.log(`Withdraw failed: ${error.message}`)
+}
+try {
+    const tran2 = new Transaction('13/01/2012', 100.00, null, 100.00);
+    account.deposit(tran2);
+} catch (error) {
+    console.log(error);
+    console.log(`Deposit failed: ${error.message}`);
+}
+
+console.log(account.getBalance());
+console.log(account.getTransactions());
