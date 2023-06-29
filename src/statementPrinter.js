a1c8d5e0aad804762873a026fa2b4f00b308548b
SaraAGithub-Portfolio <skausa29@gmail.com>
2023-06-29 14:34:02 +0100

    Had to refactor code as previous code block was 9 lines, and then had to break down code so that it followed the 5 line rule while also working correctly.

--- src/statementPrinter.js
+++ src/statementPrinter.js
@@ -1,20 +1,47 @@
 import chalk from "chalk";
+
 class StatementPrinter {
     static printArray(transactions) {
-        transactions.forEach(transaction => {
-            const date = transaction.getDate();
-            const credit = transaction.getCredit() !== null ? transaction.getCredit().toFixed(2) : '';
-            const debit = transaction.getDebit() !== null ? transaction.getDebit().toFixed(2) : '';
-            const balance = transaction.getBalance().toFixed(2);
-
-            const formattedCredit = credit ? chalk.green(credit) : '';
-            const formattedDebit = debit ? chalk.red(debit) : '';
-            const formattedBalance = balance > 0 ? chalk.green(balance) : chalk.red(balance);
-
-            console.log(`${date} || ${formattedCredit.padEnd(7)} || ${formattedDebit.padEnd(6)} || ${formattedBalance}`);
-
+        transactions.forEach((transaction) => {
+            const formattedTransaction = this.formatTransaction(transaction);
+            console.log(formattedTransaction);
         });
     }
-}
 
-export default StatementPrinter; 
+    static formatTransaction(transaction) {
+        const date = this.formatDate(transaction);
+        const formattedCredit = this.formatCredit(transaction);
+        const formattedDebit = this.formatDebit(transaction);
+        const formattedBalance = this.formatBalance(transaction);
+
+        return `${date} || ${formattedCredit.padEnd(7)} || ${formattedDebit.padEnd(6)} || ${formattedBalance}`;
+    }
+
+    static formatDate(transaction) {
+        return transaction.getDate();
+    }
+
+    static formatCredit(transaction) {
+        const credit = transaction.getCredit();
+        return credit != null ? chalk.green(credit.toFixed(2)) : '';
+    }
+
+    static formatDebit(transaction) {
+        const debit = transaction.getDebit();
+        return debit !== null ? chalk.red(debit.toFixed(2)) : '';
+    }
+
+    static formatBalance(transaction) {
+        const balance = transaction.getBalance().toFixed(2);
+        const formattedBalance = balance > 0 ? chalk.green(balance) : chalk.red(balance);
+        return formattedBalance;
+    }
+
+}
+export default StatementPrinter;
+
+
+
+
+
+
