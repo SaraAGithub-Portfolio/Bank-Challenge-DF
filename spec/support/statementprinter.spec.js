24693e46d5cae83aa032f6d161c7c4bf2ae14bab
SaraAGithub-Portfolio <skausa29@gmail.com>
2023-06-29 14:33:10 +0100

    Had to refactor tests and had to create a mock account class which I used to test the deposit/withdraw methods for the statement printer test suites, also needed to refactor the code and have the expected console log data match the same as my statement printer class.

--- spec/support/statementprinter.spec.js
+++ spec/support/statementprinter.spec.js
@@ -1,63 +1,86 @@
 import StatementPrinter from "../../src/statementPrinter.js";
-import chalk from "chalk";
+
+class MockAccount {
+    #accountBalance;
+    #transactionsArray = [];
+
+    constructor(initialBalance = 0, transactions = []) {
+        this.#accountBalance = initialBalance;
+        this.#transactionsArray = transactions;
+    }
+    getBalance() {
+        return this.#accountBalance;
+    }
+    getTransactions() {
+        return this.#transactionsArray;
+    }
+    withdraw(transaction) {
+        const debit = transaction.getDebit();
+        this.#accountBalance -= debit;
+        return this.#transactionsArray.push(transaction)
+    }
+    deposit(transaction) {
+        const credit = transaction.getCredit();
+        this.#accountBalance += credit;
+        return this.#transactionsArray.push(transaction)
+    }
+}
+
 class MockTransaction {
     date;
     credit;
     debit;
     balance;
 
-    constructor(date = '', credit = 0, debit = 0, balance = 0) {
+    constructor(date = "", credit = 0, debit = 0, balance = 0) {
         this.date = date;
         this.credit = credit;
         this.debit = debit;
         this.balance = balance;
     }
-
     getDate() {
         return this.date;
-    };
-
+    }
     getCredit() {
         return this.credit;
-    };
-
+    }
     getDebit() {
         return this.debit;
     }
+
     getBalance() {
         return this.balance;
     }
 }
-describe('Statement Printer tests', () => {
-    let logSpy;
+
+describe("Statement Printer tests", () => {
     let transactionsArray;
+    let testAccount;
 
     beforeEach(() => {
-        logSpy = spyOn(console, 'log');
+        testAccount = new MockAccount();
         transactionsArray = [
-            new MockTransaction('11/01/2012', 0, 50, 50),
-            new MockTransaction('11/11/2011', 50, 0, 100),
-            new MockTransaction('11/11/2011', 50, 0, 50),
+            new MockTransaction('11/01/2012', 0.00, 50.00, 0.00),
+            new MockTransaction('11/11/2011', 50.00, 0.00, 50.00)
         ];
+        transactionsArray.forEach((transaction) => {
+            testAccount.withdraw(transaction);
+            testAccount.deposit(transaction);
+        });
     });
-    it('should call on console.log the correct number of times', () => {
+
+    it("should call on console.log the correct number of times", () => {
+        const logSpy = spyOn(console, 'log');
         StatementPrinter.printArray(transactionsArray);
         expect(logSpy).toHaveBeenCalledTimes(transactionsArray.length);
     });
+
     it('should call console.log with the correct arguments', () => {
+        const logSpy = spyOn(console, 'log');
         StatementPrinter.printArray(transactionsArray);
+
         transactionsArray.forEach((transaction) => {
-            const date = transaction.getDate();
-            const credit = transaction.getCredit() !== null ? transaction.getCredit().toFixed(2) : '';
-            const debit = transaction.getDebit() !== null ? transaction.getDebit().toFixed(2) : '';
-            const balance = transaction.getBalance().toFixed(2);
-
-            const formattedCredit = credit ? chalk.green(credit) : '';
-            const formattedDebit = debit ? chalk.red(debit) : '';
-            const formattedBalance = balance >= 0 ? chalk.green(balance) : chalk.red(balance);
-
-
-            const expectedOutput = (`${date} || ${formattedCredit.padEnd(7)} || ${formattedDebit.padEnd(6)} || ${formattedBalance}`);
+            const expectedOutput = StatementPrinter.formatTransaction(transaction);
             expect(logSpy).toHaveBeenCalledWith(expectedOutput);
         });
     });
